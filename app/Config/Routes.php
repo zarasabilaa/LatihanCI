6b5e47ae8056d6972c38614c2a000c7134fdf448
zarasabilaa <zaraasabilaa@gmail.com>
2025-01-12 19:37:14 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\Services;
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes = Services::routes();
+
+// Load the system's routing file first, so the app and ENVIRONMENT
+// can override as needed.
+if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
+{
+    require SYSTEMPATH . 'Config/Routes.php';
+}
+
+// Define default route
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Dashboard');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+$routes->setAutoRoute(true);
+
+// Custom routes
+$routes->get('/', 'Dashboard::index');
+
+// Additional routes can be added here...
